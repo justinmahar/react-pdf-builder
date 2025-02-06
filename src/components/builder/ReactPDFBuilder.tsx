274cd24264c436c9ad3a4c5cc0937f757c3882de
Justin Mahar <contact@justinmahar.com>
2025-02-05 21:59:12 -0500

    Fix ReactPDFBuilder

--- src/components/builder/ReactPDFBuilder.tsx
+++ src/components/builder/ReactPDFBuilder.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Card, CardProps } from '../card/Card';
-import { Theme } from '../theme/Theme';
 import { CardBody, CardBodyProps } from '../card/CardBody';
 import { CardHeader, CardHeaderProps } from '../card/CardHeader';
-import { ThemeBuilder } from '../theme/ThemeBuilder';
+import { Theme } from '../theme/Theme';
 
 export class ReactPDFBuilder {
   public Card;
   public CardBody;
   public CardHeader;
-  constructor(...theme: Theme[]) {
-    const t = ThemeBuilder.mergeThemes(theme);
-    this.Card = (props: CardProps) => <Card theme={t} {...props} />;
-    this.CardBody = (props: CardBodyProps) => <CardBody theme={t} {...props} />;
-    this.CardHeader = (props: CardHeaderProps) => <CardHeader theme={t} {...props} />;
+  constructor(theme: Theme) {
+    this.Card = (props: CardProps) => <Card theme={theme} {...props} />;
+    this.CardBody = (props: CardBodyProps) => <CardBody theme={theme} {...props} />;
+    this.CardHeader = (props: CardHeaderProps) => <CardHeader theme={theme} {...props} />;
   }
 }
